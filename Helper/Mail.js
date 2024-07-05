9140e8993df202833f765bc1c275c5546335fd6f
Bharath kumar <bharathboopathi51550@gmail.com>
2024-07-05 12:40:49 +0530

    mail

--- Helper/Mail.js
+++ Helper/Mail.js
@@ -16,14 +16,14 @@ const transporter = nodemailer.createTransport({
 
 let mail = async (to, id, token) => {
     try {
-
+console.log(process.env.EMAIL);
         let mailContent = await transporter.sendMail({
             from: process.env.EMAIL,
             to: to,
             subject: 'Code to reset password',
             html: `<h1>Your Password Reset</h1>
                 <p>Click in below Link & Set the new Passwords</p>
-                <a href="${process.env.EMAIL}restart/${id}/${token}">Click here</a>
+                <a href="${process.env.EMAIL}restart/${id}/${token}">${process.env.EMAIL}restart/${id}/${token}</a>
           `
         })
 
